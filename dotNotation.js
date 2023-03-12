ff8bb167e2e061e4040762d365526dafadd7ff66
joeburnsJB <126894305+joeburnsJB@users.noreply.github.com>
2023-03-12 14:34:57 +0000

    Update dotNotation.js

--- dotNotation.js
+++ dotNotation.js
@@ -1,1 +1,14 @@
 console.log("hello".toUpperCase());
+
+const gridparts = ["    |", "---------------"];
+
+let i = 1;
+
+while (i < 12) {
+    if(i % 4 == 0)
+        console.log(gridparts[1]);
+    else
+        console.log(gridparts[0] + gridparts[0]);
+    i++;
+}
+
